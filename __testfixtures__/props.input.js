618a44557b3984a9654f7d5bee7a99af3a3cd7a2
ncarter <ncarter@madmobile.com>
2017-07-01 15:06:28 -0400

    formatting

--- __testfixtures__/props.input.js
+++ __testfixtures__/props.input.js
@@ -1,15 +1,27 @@
 const TodoItem = React.createClass({
   render: function(props) {
-    return <div>{props.item}</div>;
-  },
+    return (
+      <div>
+        {props.item}
+      </div>
+    );
+  }
 });
 
 const TodoItem = function(props) {
-  return <div>{props.item}</div>;
+  return (
+    <div>
+      {props.item}
+    </div>
+  );
 };
 
 class TodoItem extends Component {
   render(props) {
-    return <div>{props.item}</div>;
+    return (
+      <div>
+        {props.item}
+      </div>
+    );
   }
 }
