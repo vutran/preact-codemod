57a0458adb527ffca5bd519fef88ed7506544f31
ncarter <ncarter@madmobile.com>
2017-07-01 14:51:45 -0400

    updated

--- transforms/props.js
+++ transforms/props.js
@@ -2,39 +2,38 @@
  * Makes props available as the argument in its calling function.
  * Also strips the `this` context in the JSX expression.
  */
-module.exports = (file, api) => {
-  const j = api.jscodeshift;
-  const root = j(file.source);
+module.exports = ({ source }, { jscodeshift: j }) => {
+  const root = j(source);
 
   // strip this context and add the props to the function params
   root
-  	.find(j.ThisExpression)
-  	.closest(j.MemberExpression)
-    .filter(n => n.value.object.type === 'ThisExpression' && n.value.property.name === 'props')
+    .find(j.ThisExpression)
     .closest(j.MemberExpression)
-    .replaceWith(n => {
-        const prop = n.value.property.name;
-        const newNode = {
-          type: 'MemberExpression',
-          object: {
-            type: 'Identifier',
-            name: 'props',
-          },
-          property: {
-            type: 'Identifier',
-            name: prop,
-          },
-        };
-        if (prop) {
-          j(n)
-            .closest(j.FunctionExpression)
-            .forEach(n1 => {
-              if (!n1.value.params.length) {
-                n1.value.params.push('props');
-              }
-            });
+    .filter(
+      ({ value: { object: { type }, property: { name } } }) =>
+        type === 'ThisExpression' && name === 'props'
+    )
+    .closest(j.MemberExpression)
+    .filter(j.MemberExpression, { name: 'props' })
+    .replaceWith(({ value: { property: { name: prop } } }) => {
+      if (prop) {
+        j(n).closest(j.FunctionExpression).forEach(({ value: { params: { length }, params } }) => {
+          if (!length) {
+            params.push('props');
+          }
+        });
+      }
+      return {
+        type: 'MemberExpression',
+        object: {
+          type: 'Identifier',
+          name: 'props'
+        },
+        property: {
+          type: 'Identifier',
+          name: prop
         }
-        return newNode;
+      };
     });
 
   return root.toSource();
