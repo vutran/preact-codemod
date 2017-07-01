bf4208d77cfa972efbfa9ec38420921c0b9d0c8e
ncarter <ncarter@madmobile.com>
2017-07-01 15:01:56 -0400

    prevent props from being re-added if already exists

--- transforms/props.js
+++ transforms/props.js
@@ -2,28 +2,19 @@
  * Makes props available as the argument in its calling function.
  * Also strips the `this` context in the JSX expression.
  */
-module.exports = ({ source }, { jscodeshift: j }) => {
-  const root = j(source);
+module.exports = (file, api) => {
+  const j = api.jscodeshift;
+  const root = j(file.source);
 
   // strip this context and add the props to the function params
   root
     .find(j.ThisExpression)
     .closest(j.MemberExpression)
-    .filter(
-      ({ value: { object: { type }, property: { name } } }) =>
-        type === 'ThisExpression' && name === 'props'
-    )
+    .filter(n => n.value.object.type === 'ThisExpression' && n.value.property.name === 'props')
     .closest(j.MemberExpression)
-    .filter(j.MemberExpression, { name: 'props' })
-    .replaceWith(({ value: { property: { name: prop } } }) => {
-      if (prop) {
-        j(n).closest(j.FunctionExpression).forEach(({ value: { params: { length }, params } }) => {
-          if (!length) {
-            params.push('props');
-          }
-        });
-      }
-      return {
+    .replaceWith(n => {
+      const prop = n.value.property.name;
+      const newNode = {
         type: 'MemberExpression',
         object: {
           type: 'Identifier',
@@ -34,6 +25,14 @@ module.exports = ({ source }, { jscodeshift: j }) => {
           name: prop
         }
       };
+      if (prop) {
+        j(n).closest(j.FunctionExpression).forEach(n1 => {
+          if (!n1.value.params.length || !n1.value.params.contains('props')) {
+            n1.value.params.push('props');
+          }
+        });
+      }
+      return newNode;
     });
 
   return root.toSource();
